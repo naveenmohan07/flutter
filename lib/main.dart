db70f00e97047fdc26e93daa3b87cf68ab2e9970
Naveen Mohan <nmohanasundaram@biolytica.com>
2023-10-28 22:30:11 +0530

    feat: firebase remote config

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,14 @@ import 'package:boiler_plate/apps/user.app.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
+import 'package:shared/shared.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
+  
+ FirebaseConfigService().initilize();
   runApp(const MainApp());
 }
 
@@ -18,10 +21,17 @@ class MainApp extends StatefulWidget {
 }
 
 class _MainAppState extends State<MainApp> {
+  String val = "";
+@override
+  void initState() {
+    val = FirebaseConfigService().getString("APP_THEME");
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: UserApp(),
+    return MaterialApp(
+      theme: val =="light" ? ApplicationTheme.lightTheme : ApplicationTheme.darkTheme,
+      home: const UserApp(),
     );
   }
 }
--- packages/shared/lib/shared.dart
+++ packages/shared/lib/shared.dart
@@ -9,4 +9,11 @@ export 'src/screens/maintenance.screen.dart';
 export 'src/screens/permission.denied.screen.dart';
 export 'src/screens/splash.screen.dart';
 export 'src/screens/unauthorized.screen.dart';
+
+export 'src/constants/color.constant.dart';
+export 'src/constants/firebase.constant.dart';
+
+export 'src/themes/theme.dart';
+
+export 'src/services/firebase.config.service.dart';
 // TODO: Export any libraries intended for clients of this package.
